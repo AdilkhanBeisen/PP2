8b071ea703cf8b2bea8a7983522b737b4fb718f8
AdilkhanBeisen <adilhanbeisen@gmail.com>
2026-01-27 10:39:39 +0500

    Add Practice2 - Python control flow examples if-else

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,21 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+  sername = "Emil"
+
+if len(sername) > 0:
+  print(f"Welcome, {sername}!")
+else:
+  print("Error: Username cannot be empty")
